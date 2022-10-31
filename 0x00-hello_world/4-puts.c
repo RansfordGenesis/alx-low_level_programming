596d5b624219882629c3eff8368334cb5a3ded9a
AI-Amponsah <Yawamp27@gmail.com>
2022-10-31 04:56:57 +0000

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - This program displays a string.
+  *Main prints out a string using puts
+  *Return:   no returns.
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - prints a text.
+  *
+  *Return: Always .O
+  */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
